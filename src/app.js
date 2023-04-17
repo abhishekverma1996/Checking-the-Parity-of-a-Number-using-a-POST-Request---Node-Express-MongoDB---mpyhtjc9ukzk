8d948f2db76451fd2cdbba10b2d44586d3efbb93
Abhishek Verma <43173187+abhishekverma1996@users.noreply.github.com>
2023-04-17 22:09:56 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -12,6 +12,19 @@ const server = http.createServer((req, res) => {
       const value = obj.num1;
     
      // Write the code here to check if the number is odd or even
+      if (isNaN(value)) {
+        // res.statusCode = 400;
+        res.writeHead(400, { "Content-Type": "text/plain" });
+        res.end(`${value} is not a number`);
+      } else if (value % 2 === 0) {
+        // res.statusCode = 200;
+        res.writeHead(200, { "Content-Type": "text/plain" });
+        res.end(`The number ${value} is even`);
+      } else if (value % 2 !== 0) {
+        // res.statusCode = 404;
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.end(`The number ${value} is odd`);
+      }
 
    });
   }
